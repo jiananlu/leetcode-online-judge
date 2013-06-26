aa70f14af33c19f4f02b888b41341ce1b88f1a18
Jianan Lu <jianan.lu@salesforce.com>
2013-06-26 12:17:59 -0700

    add problem description to palindrome-partitioning-2

--- palindrome-partitioning-2.java
+++ palindrome-partitioning-2.java
@@ -1,5 +1,14 @@
 // small passed; large not passed
 
+/* Problem:
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return the minimum cuts needed for a palindrome partitioning of s.
+
+For example, given s = "aab",
+Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+*/
+
 public class Solution {
     public int minCut(String s) {
         return minCut(s, 0);
